41176b3be80ec9ba366bc349d22d780f51f3412b
Bora Guven <guvenbe@gmail.com>
2020-12-03 23:39:59 -0500

    init commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+https://javagyansite.com/2018/12/27/functional-interfaces-predicate-consumer-function-and-supplier/
--- src/main/java/functional/lambda/LambdaMultiLine.java
+++ src/main/java/functional/lambda/LambdaMultiLine.java
@@ -7,7 +7,7 @@ public class LambdaMultiLine {
     public static void main(String[] args) {
         LengthOfString2 ls =(s)->{
             int len = s.length();
-            System.out.println(len);
+            System.out.println("Length of the string=" + len);
             return len;
         };
         ls.apply("Row, Row your baot");
