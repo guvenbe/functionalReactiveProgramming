729aece1db9b32665891ef796e5b3642b218d865
Bora Guven <guvenbe@gmail.com>
2020-12-05 13:35:05 -0500

    init commit

--- src/main/java/com/basicsstring/generics/RawTypeDemo.java
+++ src/main/java/com/basicsstring/generics/RawTypeDemo.java
@@ -1,5 +1,6 @@
 package com.basicsstring.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Box<T>{
@@ -18,6 +19,12 @@ class Box<T>{
     }
 }
 
+class SquareBox<T> extends Box<T>{
+
+    public SquareBox(List<T> a) {
+        super(a);
+    }
+}
 public class RawTypeDemo {
     public static void main(String[] args) {
         Box<Integer> genBox = new Box<>(List.of(1,2,3));
@@ -27,6 +34,20 @@ public class RawTypeDemo {
 
         //Avoid this maybe not type safe
         Box rawBox = new Box(List.of(1,2,3,"My String")); // <-- No restriction
+
+        List<Number> list = new ArrayList<>();
+        list.add(Integer.valueOf(10));
+        list.add(Double.valueOf(10.5));
+
+        List<Box<Integer>> boxList = new ArrayList<>();
+        boxList.add(new Box<>(List.of(1,2,3)));
+        boxList.add(new Box<>(List.of(6,2,3)));
+        boxList.add(new SquareBox<>(List.of(4,5,6)));
+
+        //Subtyping only work if Type is the same
+        //List<Number> l1 = new ArrayList<Integer>(); //<--Does not work
+        //ArrayList<Number> l1 = new ArrayList<Integer>(); //<--Does not work
+        List<Integer> l1 = new ArrayList<Integer>();
 
 
     }
--- src/main/java/com/basicsstring/generics/SubtypingDemo.java
+++ src/main/java/com/basicsstring/generics/SubtypingDemo.java
@@ -1,4 +1,22 @@
 package com.basicsstring.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubtypingDemo {
+
+    public static void main(String[] args) {
+
+        Object o = new Object();
+        Integer i =Integer.valueOf(1);
+        o=i;
+        System.out.println(o);
+
+        List<Number> list = new ArrayList<>();
+        list.add(Integer.valueOf(10));
+        list.add(Double.valueOf(10.5));
+        //list.add("hello")) <-- error not compatible not a child or subtype
+
+
+    }
 }
